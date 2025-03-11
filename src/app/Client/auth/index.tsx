2039fae3487848be06e3d4e41f3cbd7a79b0f953
sabi-hawk <miansabby516@gmail.com>
2025-03-12 00:14:54 +0500

    latest

--- src/app/Client/auth/index.tsx
+++ src/app/Client/auth/index.tsx
@@ -4,7 +4,7 @@ import Login from "./login";
 import { useParams } from "react-router-dom";
 
 function ClientAuth() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   const { providerId } = useParams<{ providerId: string }>() as {
     providerId: string;
   };
