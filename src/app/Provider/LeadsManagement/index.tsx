64f85594e069424c806c90ff956aa1a239557ec3
sabi-hawk <miansabby516@gmail.com>
2025-03-24 01:33:21 +0500

    misc

--- src/app/Provider/LeadsManagement/index.tsx
+++ src/app/Provider/LeadsManagement/index.tsx
@@ -81,7 +81,13 @@ const data: DataType[] = Array.from({ length: 50 }).map((_, i) => ({
   description: `This is group ${i + 1}.`,
 }));
 
+const handleEdit = (key: React.Key) => {
+  console.log("Edit record", key);
+};
 
+const handleDelete = (key: React.Key) => {
+  console.log("Delete record", key);
+};
 
 function LeadsManagement() {
   return (
