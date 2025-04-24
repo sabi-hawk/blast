25b8a8d963b119ba1fcca32978ba863a5ade8542
sabi-hawk <miansabby516@gmail.com>
2025-04-24 15:16:58 +0500

    fix

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -6,7 +6,22 @@ import { clearUser, setUser } from "../flux/reducers/auth";
 
 const API = axios.create({ baseURL: "http://localhost:8000/api" });
 
+API.interceptors.request.use(
+  (config) => {
+    const {
+      auth: { token },
+    } = store.getState();
+    if (token && config.headers) {
+      config.headers["auth-token"] = token;
+    }
 
+    return config;
+  },
+  (error) => {
+    console.log("API Request | Interceptor |  Error", error);
+    return Promise.reject(error);
+  }
+);
 API.interceptors.response.use(
   (res) => {
     return res;
